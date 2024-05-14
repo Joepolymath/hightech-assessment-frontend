e94c31dc559770d11d4253fd02b38f004154cd01
Joepolymath <joshuaajagbe96@gmail.com>
2024-05-14 04:27:44 +0100

    email list component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,15 @@
 import ContentHeader from './ContentHeader';
+import EmailDetails from './EmailDetails';
+import EmailList from './EmailList';
 
 const Main = () => {
   return (
     <main className="flex flex-col w-full bg-dark-600 h-full">
       <ContentHeader />
+      <div className="flex flex-row" style={{ height: 'calc(100% - 84px)' }}>
+        <EmailList />
+        <EmailDetails />
+      </div>
     </main>
   );
 };
